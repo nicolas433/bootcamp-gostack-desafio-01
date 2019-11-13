cdee135ceb89960c1430d5e2cf24298e0cb422fe
nicolas433 <ngrisoste@gmail.com>
2019-11-13 12:41:49 -0300

    Cadastro de tarefas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+/* 
+ Query params = ?parametro={valor}
+    req.query
+ Route params = /users/{valor}
+    req.params
+ Request body = { "post": "true", 
+                  "put": "true", 
+                  "get": "false", 
+                  "delete": "false" }
+*/
+
 const express = require('express');
 const server = express();
 
+server.use(express.json());
 
-server.listen(3000, () => console.log("Server on!!!"));
+let data = [{
+}]
+
+server.post('/projects', (req, res) =>{
+    const new_data = {id, title, tasks} = req.body;
+    data.push(new_data);
+    res.json({status: "Tarefa armazenada com sucesso!!!"})
+})
+
+
+server.get('/projects', (req, res) =>{
+    res.send("teste");
+})
+
+server.listen(3003, () => console.log("Server on!!!"));
