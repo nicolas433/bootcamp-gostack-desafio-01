c06f4bcd9931123c897201389f6daa0495ebe87a
nicolas433 <ngrisoste@gmail.com>
2019-11-13 12:59:53 -0300

    Atualizar titulo de projeto

--- src/index.js
+++ src/index.js
@@ -11,20 +11,29 @@
 
 const express = require('express');
 const server = express();
-
 server.use(express.json());
 
 let data = [{
+    "id": "id da tarefa",
+    "title": "titulo da tarefa",
+    "tasks": "vetor de tarefas"
 }]
 
 server.post('/projects', (req, res) =>{
     const new_data = {id, title, tasks} = req.body;
     data.push(new_data);
-    res.json({status: "Tarefa armazenada com sucesso!!!"})
+    res.json({status: "Tarefa armazenada com sucesso!!!"});
 })
 
 server.get('/projects', (req, res) =>{
     res.json(data);
+})
+
+server.put('/projects/:id', (req, res) =>{
+    console.log(req.params.id);
+    data[req.params.id].title = req.body.title;
+    req.params
+    res.json({status: "Tarefa armazenada com sucesso!!!"});
 })
 
 
